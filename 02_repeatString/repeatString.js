72c807884ceffe9602c66979238822c86463c9db
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-10 20:11:40 +0300

    Completed 02_RepeatString Task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let text = "";
 
+  for (i = 0; i < num; i++){
+    text += string
+  }
+  
+  if(num < 0){
+    text = "ERROR";
+  }
+
+  return text
 };
 
 // Do not edit below this line
