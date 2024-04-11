8f793a54db354d833ebf06504107d64293900f9c
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-11 18:18:05 +0300

    completed 04_RemoveFromArray Task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...extract) {
+  let myNewArray = [];
+  for(i = 0; i < myArray.length; i++){
+    if(extract.includes(myArray[i])) continue;
+    myNewArray.push(myArray[i]);
+  }
+  return myNewArray;
 };
 
 // Do not edit below this line
