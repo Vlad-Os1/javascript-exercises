a2b359a8ed6503a72e822eca48c17bead695b06e
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-11 20:57:08 +0300

    Completed 05_SUMALL Task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+    return 'ERROR';
+  }
+  if(num1 < 0 || num2 < 0){
+    return 'ERROR';
+  }
+  if(num1 > num2){
+    let sum = num1 + num2;
+    let final = 0;
+    for(i = 0; i < sum; i++){
+      final += i;
+    }
+    return final;
+  }
 
+  if(num2 > num1){
+    let sum = num2 + num1;
+    let final = 0;
+    for(i = 0; i < sum; i++){
+      final += i;
+    }
+    return final;
+  }
 };
 
 // Do not edit below this line
