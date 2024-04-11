c8e85bf32ca8e6744fb2a8e47489d9f973232643
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-11 16:26:52 +0300

    Completed 03-ReverseString task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  let reversedString = '';
+  for(let i = text.length - 1 ; i >= 0; i-- ){
+    reversedString += text[i];
+  }
+  return reversedString;
 
+
+  // Also an interesting solution without using loop
+  // const reversedStr = text.split('').reverse().join('');
+  // return reversedStr
 };
 
 // Do not edit below this line
