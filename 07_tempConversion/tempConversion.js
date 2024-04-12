290ae9e8c52b5f66aba7812b36ef95f45c279e95
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-04-12 19:42:55 +0300

    Completed 07_TempConversion Task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let convertedValue = (degree - 32) / 1.8;
+  if (!Number.isInteger(convertedValue)){
+    return convertedValue = parseFloat(convertedValue.toFixed(1));
+  }
+  return convertedValue; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let convertedValue = (degree * 1.8) + 32;
+  if(!Number.isInteger(convertedValue)){
+    return convertedValue = parseFloat(convertedValue.toFixed(1));
+  }
+  return convertedValue;
 };
 
 // Do not edit below this line
