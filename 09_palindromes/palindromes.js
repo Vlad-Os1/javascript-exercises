54d686b0b398a21595bc6c98aad9acad4a087275
Vlad-Os1 <oshovskiyvlad28@gmail.com>
2024-05-03 02:39:21 +0300

    complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  let array = text.toLowerCase().split("").filter((item) => alphanumerical.includes(item)).join("");
+  let reversedText = array.split("").reverse().join('');
+
+  return array == reversedText;
 };
 
 // Do not edit below this line
